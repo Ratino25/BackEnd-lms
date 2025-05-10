f21814d325d11a146f13616ae242917023d5bad1
Ratino <ratinoray@gmail.com>
2025-05-10 17:18:05 +0700

    preparation backEnd

---
+++ src/middlewares/validateRequest.js
@@ -0,0 +1,14 @@
+import { ZodError } from "zod"
+
+export const validateRequest = (schema) => async (req, res, next) => {
+    try {
+        schema.parse(req.body)
+        next()
+    } catch (error) {
+        if(error instanceof ZodError){
+            const errorMessage = error.issues.map((err => err.message))
+            return res.status(500).json({error : 'Invalid Request', details: errorMessage})
+        }
+        res.status(500).json({error : 'Internal Server'})
+    }
+}
